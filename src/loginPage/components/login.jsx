a49e71f245b09d6b70f6f2a2f2d67f8cce8e65be
Chanyang Choi <cychoi710@icloud.com>
2023-02-21 16:44:05 +0900

    2nd day created and organized files

---
+++ src/loginPage/components/login.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./login.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="heading">TeamGram</div>
+        <form className="login-form">
+          <div className="field">
+            <input id="username" type="name" placeholder="Username or email"/> 
+            <label for="username">Username or email</label>
+          </div>
+          <div className="field">
+            <input id="password" type="password" placeholder="password" />
+            <label for="password">Password</label>
+          </div>
+          <button className="login-button" title="login">Log In</button>
+          <div className="other">
+          <a className="forgot-password" href="#">Forgot password?</a>
+          </div>
+        </form>
+      </div>
+      <div class="box">
+        <p>Don't have an account? <a className="signup" href="#">Sign Up</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './loginPage/login'
+import App from './loginPage/components/login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
