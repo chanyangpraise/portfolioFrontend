6615cbb9208df25506a38332921e48d5c9160419
hodong <jeonghodong@naver.com>
2023-02-22 14:39:58 +0900

    프로젝트 3일차

--- src/Routers/RightMain.jsx
+++ src/Routers/RightMain.jsx
@@ -5,32 +5,49 @@ import SideProfile from "../Components/SideProfile";
 
 function RightMain() {
   const textarea = useRef();
+  const imgbutton = useRef();
   const [text, setText] = useState();
   const [texts, setTexts] = useState([]);
+  const [img, setImg] = useState();
+  const [imgs, setImgs] = useState([]);
 
   const TextAdd = (event) => {
-    console.log(event.target.value);
     setText(event.target.value);
 
-    // textarea ResizeHeight
+    // textarea ResizeHeight //
     textarea.current.style.height = "auto"; //height 초기화
     textarea.current.style.height = textarea.current.scrollHeight + "px";
   };
 
+  // submit //
   const onSubmit = (event) => {
     // onSubmit 새로고침 안되게
     event.preventDefault();
     setText("");
+    setImg("");
     // Submit 했을때 textarea height 초기화
     textarea.current.style.height = "auto";
     // textarea의 값이 빈 값일때 Submit 안되게
     if (text === "") {
       return;
     }
-
+    // 배열에 담기
+    //
+    //
     //
     setTexts((current) => [text, ...current]);
     console.log(texts);
+    setImgs((current) => [imgs, ...current]);
+  };
+
+  // img add //
+  const ImgAdd = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onloadend = () => {
+      setImg(reader.result);
+    };
+    console.log(e.target.files[0]);
   };
 
   return (
@@ -49,17 +66,33 @@ function RightMain() {
             rows={1}
             value={text}
           />
+          <img src={img} onerror="this.style.display='none'" className="main_input_img" />
         </div>
         <div className="main_bottom_box">
-          <img src={imgButton} alt="imgButton" className="main_img_button" />
+          <input
+            src={imgButton}
+            type="file"
+            alt="imgButton"
+            className="main_img_button"
+            accept="image/*"
+            ref={imgbutton}
+            onChange={ImgAdd}
+          />
           <button className="main_send_button">Send</button>
         </div>
       </form>
-      <ul>
+      <ul className="main_post_wrap">
         {texts.map((v) => (
           <Fragment key={v}>
             <SideProfile />
-            <li>{v}</li>
+            <li style={{ wordBreak: "break-word" }}>
+              {v}
+              {imgs.map((v) => {
+                {
+                  v;
+                }
+              })}
+            </li>
           </Fragment>
         ))}
       </ul>
