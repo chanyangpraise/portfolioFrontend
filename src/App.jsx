6600c9b863d694b66fcb3f28d0b76b84cf3f6436
pineconekr <solbin2255@naver.com>
2023-02-22 15:42:58 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./reset.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SideBar from "./Routers/SideBar";
+import TestProfile from "./Routers/TestProfile";
+import RightMain from "./Routers/RightMain";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<SideBar />}>
+          <Route path="/" element={<RightMain />} />
+          <Route path="/profile" element={<TestProfile />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+export default App;
