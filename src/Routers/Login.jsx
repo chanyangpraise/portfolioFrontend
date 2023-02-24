00f55962682f14bb2d135b3ca1ba9955a6196a17
Chanyang Choi <cychoi710@icloud.com>
2023-02-24 11:45:53 +0900

    로그인 이미지 추가, 로그인 첫페이지 디자인

--- src/Routers/Login.jsx
+++ src/Routers/Login.jsx
@@ -1,43 +1,57 @@
 import React from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
+import phones from "../asset/loginimg/phones.png"
+import picture1 from "../asset/loginimg/photo-1.png"
+import picture2 from "../asset/loginimg/photo-2.png"
+import picture3 from "../asset/loginimg/photo-3.png"
+import picture4 from "../asset/loginimg/photo-4.png"
+import picture5 from "../asset/loginimg/photo-5.png"
+import logo from "../asset/loginimg/Teamstagramlogo.png"
 
 function Login() {
   return (
-    <div className="container">
-      <div className="box">
-        <div className="heading">TeamGram</div>
-        <form className="login-form">
-          <div className="field">
-            <input id="username" type="name" placeholder="Username or email" />
-            <label for="username">Username or email</label>
-          </div>
-          <div className="field">
-            <input id="password" type="password" placeholder="password" />
-            <label for="password">Password</label>
-          </div>
-          <Link to="/main">
-            <button className="login-button" title="login">
-              Log In
-            </button>
-          </Link>
-          <div className="other">
-            <a className="forgot-password" href="#">
-              Forgot password?
-            </a>
-          </div>
-        </form>
-      </div>
-      <div class="box">
-        <p>
-          Don't have an account?{" "}
-          <a className="signup" href="#">
-            Sign Up
-          </a>
-        </p>
-      </div>
-    </div>
-  );
-}
+    <div>
+    <div id="content-container">
+            {/*Phone's pictures section*/}
+  <div className="phones">
+  <img src={phones} alt="pictures on phone" className="phone-image" />
+            <div className="display-phone">
+                <img className="picture" src={picture1} alt="#" />
+                <img className="picture" src={picture2} alt="#" />
+                <img className="picture" src={picture3} alt="#" />
+                <img className="picture" src={picture4} alt="#" />
+                <img className="picture" src={picture5} alt="#" />
+            </div>
+  </div>
+            {/*User's log in section*/}
+            <div className="user">
+              <div className="login-container">
+                  {/*Instagram Logo*/}
+                  <div className="instagram-logo-box">
+                    <img className="instagram-logo" src={logo} />
+                  </div>
+                  {/*Form Login*/}
+                  <form id="login-post" method="POST">
+                  <div className="inputs-container">
+                      <input type="text" name="username" placeholder="Phone number, username or email" />
+                  </div>
+                  <div className="inputs-container">
+                      <input type="password" name="password" placeholder="Password" />
+                  </div>
+                  <a className="login-button" href="#" target="_blank">Log In</a>
+                  </form>
+                  {/*Password recovery*/}
+                  <a className="password-forgot" href="#" target="_blank">Forgot password?</a>
+              </div>
+              {/*Sign up*/}
+              <div className="signup-container">
+                  <p>Don't have an account? <a className="signup" href="#" target="_blank">Sign up</a></p>
+              </div>
+            </div>
+        </div>
+        </div>
+    );
+    }
 
 export default Login;
