f9ca11138d8d89aa820993177cb497c45a28b107
hodong <jeonghodong@naver.com>
2023-02-28 15:01:28 +0900

    style 변경

--- src/Components/Main/MainComment.jsx
+++ src/Components/Main/MainComment.jsx
@@ -3,10 +3,21 @@ import SideProfile from "../Side/SideProfile";
 import "./MainComment.css";
 
 function MainComment({ v, i }) {
+  function removeView() {
+    if (window.confirm("댓글을 삭제하시겠습니까?")) {
+      console.log("삭제완료");
+    }
+  }
   return (
-    <div key={i} style={{ borderTop: "1px solid rgb(194, 194, 194)" }}>
-      <SideProfile />
-      <pre style={{ wordBreak: "break-word", whiteSpace: "pre-line", marginBottom: "2rem" }}>{v}</pre>
+    <div className="main_comment_wrap" key={i} style={{ borderTop: "1px solid rgb(194, 194, 194)" }}>
+      <div>
+        <SideProfile />
+        <pre style={{ wordBreak: "break-word", whiteSpace: "pre-line", marginBottom: "2rem" }}>{v}</pre>
+      </div>
+      <div className="main_comment_menu">
+        <span style={{ marginRight: "1rem" }}>수정</span>
+        <span onClick={() => removeView()}>삭제</span>
+      </div>
     </div>
   );
 }
