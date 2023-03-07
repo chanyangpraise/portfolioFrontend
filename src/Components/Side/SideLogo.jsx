5fc9e9e00f7cd23ecc613341f3f16cc7ae06da36
hodong <jeonghodong@naver.com>
2023-03-07 15:48:28 +0900

    로고링크 수정

--- src/Components/Side/SideLogo.jsx
+++ src/Components/Side/SideLogo.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import './SideLogo.css';
+import React from "react";
+import "./SideLogo.css";
 
 function SideLogo() {
   return (
     <div>
       <a href="/main">
-        <img
-          className="sidebar_header"
-          src="/src/asset/Teamstagramlogo.png"
-        ></img>
+        <img className="sidebar_header" src="../src/asset/Teamstagramlogo.png"></img>
       </a>
     </div>
   );
